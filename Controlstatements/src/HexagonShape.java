a282bbd911319c804334be4396df61526a4d9250
Likitha <likitha7755@gmail.com>
2022-09-29 19:59:42 +0530

    test

--- Controlstatements/src/HexagonShape.java
+++ Controlstatements/src/HexagonShape.java
@@ -1,5 +1,5 @@
 public class HexagonShape {
-    public static void main(String args[]){
+    public static void main(String []args){
         for(int i=1; i<=13; i++){
             for(int j=1; j<=13; j++){
                 if(i==3 && j==5 || i==3 && j==6 || i==3 && j==7 || i==3 && j==8 || i==3 && j==9 || i==4 && j==4 || i==4 && j==10||
--- Controlstatements/src/PentagonShape.java
+++ Controlstatements/src/PentagonShape.java
@@ -1,5 +1,5 @@
 public class PentagonShape {
-    public static void main(String args[]){
+    public static void main(String []args){
        int i=1;
        do{
            int j=1;
