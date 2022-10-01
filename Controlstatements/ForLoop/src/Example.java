89babb4a4a13652418d5ec5b897b0ca05b01cf40
Likitha <likitha7755@gmail.com>
2022-10-01 17:36:09 +0530

    test

---
+++ Controlstatements/ForLoop/src/Example.java
@@ -0,0 +1,17 @@
+public class Example {
+    public static void main(String []args) {
+        int t = 5;
+        for (int i = 1; i <= 5; i++) {
+            t = t * i;
+            System.out.println(t);
+            //  for(int i =1; i<100; i++){
+            //    System.out.println(i);
+
+        }
+    }
+
+       /* syntax for loop
+            for(initialization; condition; increment/decrement){
+            //body
+        */
+}
